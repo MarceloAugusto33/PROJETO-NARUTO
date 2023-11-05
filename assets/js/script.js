13fb057f8ddf0ce9a03f452e65db2a9b568c0b56
Marcelo Augusto Tavares <132106588+MarceloAugusto33@users.noreply.github.com>
2023-11-05 15:24:11 -0300

    Personagens add

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+let cardNaruto = document.querySelector('#naruto');
+let cardSasuke = document.querySelector('#sasuke');
+let cardSakura = document.querySelector('#sakura');
+let cardKakashi = document.querySelector('#kakashi');
+let cardMadara = document.querySelector('#madara');
+let cardMinato = document.querySelector('#minato');
+
+let img = document.querySelector('#personagem');
+let nomePersonagem = document.querySelector('#nome-perso')
+let descPersonagem = document.querySelector('#desc-perso')
+let corpo = document.querySelector('#container-personagem')
+
+cardNaruto.addEventListener('click',function() {
+    img.setAttribute('src','assets/images/NARUTO-CORPOCOMPLETO.webp');
+    nomePersonagem.textContent = "Naruto Uzumaki";
+    nomePersonagem.style.color = "#FF6F00";
+    descPersonagem.textContent = "O protagonista da série, conhecido por seu desejo de se tornar Hokage e por sua determinação inabalável. Ele possui uma natureza otimista e a habilidade de usar o chakra da raposa de nove caudas, Kurama.";
+    corpo.classList.remove('sasuke');
+    corpo.classList.remove('sakura');
+    corpo.classList.remove('kakashi');
+    corpo.classList.remove('madara');
+    corpo.classList.remove('minato');
+    corpo.classList.add('naruto');
+})
+cardSasuke.addEventListener('click',function() {
+    img.setAttribute('src','assets/images/SASUKE-CORPOCOMPLETO.webp');
+    nomePersonagem.textContent = "Uchiha Sasuke";
+    nomePersonagem.style.color = "#005CB2";
+    descPersonagem.textContent = "Um dos últimos sobreviventes do clã Uchiha, rival de Naruto e amigo de infância. Ele busca vingança pela destruição de seu clã e passa por uma jornada de autodescoberta e redenção ao longo da série.";
+    corpo.classList.remove('sakura');
+    corpo.classList.remove('kakashi');
+    corpo.classList.remove('madara');
+    corpo.classList.remove('minato');
+    corpo.classList.remove('naruto');
+    corpo.classList.add('sasuke');
+})
+cardSakura.addEventListener('click',function() {
+    img.setAttribute('src','assets/images/SAKURA-CORPOCOMPLETO.webp');
+    nomePersonagem.textContent = "Sakura Haruno";
+    nomePersonagem.style.color = "#FF69B4 ";
+    descPersonagem.textContent = "Uma kunoichi talentosa e membro do Time 7. Inicialmente focada em Sasuke, Sakura evolui como ninja, desenvolvendo suas habilidades médicas e se tornando uma forte e inteligente combatente";
+    corpo.classList.remove('kakashi');
+    corpo.classList.remove('madara');
+    corpo.classList.remove('minato');
+    corpo.classList.remove('naruto');
+    corpo.classList.remove('sasuke');
+    corpo.classList.add('sakura');
+})
+cardKakashi.addEventListener('click',function() {
+    img.setAttribute('src','assets/images/KAKASHI-CORPOCOMPLETO.webp')
+    nomePersonagem.textContent = "Hatake Kakashi"
+    nomePersonagem.style.color = "#02376b"
+    descPersonagem.textContent = "O líder do Time 7, conhecido por sua máscara e por ser um mestre ninja. Ele é habilidoso em diversas técnicas, incluindo o Chidori e o Sharingan."
+    corpo.classList.remove('sakura')
+    corpo.classList.add('kakashi')
+    corpo.classList.remove('madara')
+    corpo.classList.remove('minato')
+    corpo.classList.remove('naruto')
+    corpo.classList.remove('sasuke')
+})
+cardMadara.addEventListener('click',function() {
+    img.setAttribute('src','assets/images/MADARA-CORPOCOMPLETO.webp')
+    nomePersonagem.textContent = "Uchiha Madara"
+    nomePersonagem.style.color = "#8B0000 "
+    descPersonagem.textContent = "Um lendário shinobi e um dos antagonistas principais da série. Ele é conhecido por sua busca pelo poder e por seu papel fundamental na história dos clãs ninja."
+    corpo.classList.remove('sakura')
+    corpo.classList.remove('kakashi')
+    corpo.classList.add('madara')
+    corpo.classList.remove('minato')
+    corpo.classList.remove('naruto')
+    corpo.classList.remove('sasuke')
+})
+cardMinato.addEventListener('click',function() {
+    img.setAttribute('src','assets/images/MINATO-CORPOCOMPLETO.webp')
+    nomePersonagem.textContent = "Namikaze Minato"
+    nomePersonagem.style.color = "#FFD700  "
+    descPersonagem.textContent = "O Quarto Hokage de Konoha e pai de Naruto. Ele é famoso por suas habilidades como ninja, especialmente por ser o usuário original da técnica do Rasengan e por ser conhecido como o Relâmpago Amarelo de Konoha."
+    corpo.classList.remove('sakura')
+    corpo.classList.remove('kakashi')
+    corpo.classList.remove('madara')
+    corpo.classList.add('minato')
+    corpo.classList.remove('naruto')
+    corpo.classList.remove('sasuke')
+})
