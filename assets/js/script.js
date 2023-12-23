e29c2f6560310a875d6dbeefa4664b15ffd56de3
Marcelo Augusto Tavares <132106588+MarceloAugusto33@users.noreply.github.com>
2023-12-23 13:05:31 -0300

    Refatorando o codigo um pouco

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,49 +11,50 @@ let descPersonagem = document.querySelector('#desc-perso')
 let corpo = document.querySelector('#container-personagem')
 let fimcorpo = document.querySelector('.fim-personagens')
 
-let pointer = document.querySelector('.pointer')
 
 let body = document.querySelector('body')
 
 let links = document.querySelector("#sair")
 
-function openNav(){
+function openNav() {
     document.querySelector('.overlay').style.width = "80%"
 }
-function closeNav(){
+function closeNav() {
     document.querySelector('.overlay').style.width = "0%"
 }
 
-links.addEventListener('click',function(){
-    setTimeout(function() {
+links.addEventListener('click', function () {
+    setTimeout(function () {
         document.querySelector('.overlay').style.width = "0%"
-    },200)
+    }, 200)
 })
 
-function netflix() {
+function linkNetflix() {
     location.href = "https://www.netflix.com/br/title/80000603"
 }
 
-function crun() {
+function linkCrunchyroll() {
     location.href = "https://www.crunchyroll.com/pt-br/series/GYQ4MW246/naruto-shippuden?utm_source=google&utm_medium=paid_cr&utm_campaign=CR-Paid_Google_Web_Consideration_LATAM_BR_Generic-Titles_SVOD&utm_term=naruto&referrer=google_paid_cr_CR-Paid_Google_Web_Consideration_LATAM_BR_Generic-Titles_SVOD&gclid=Cj0KCQjw-pyqBhDmARIsAKd9XIMeazBGyaiPVXohqvCHyq0gUJ1a6P_jOI8v-2W25Lfkgj0PD-y2EI0aAnI_EALw_wcB"
 }
 
-document.addEventListener('scroll',rolar);
+
 function rolar() {
+    let pointer = document.querySelector('.btn-topo')
     let atual_posicao = window.scrollY;
-    if (atual_posicao > 300){
+    if (atual_posicao > 300) {
         pointer.style.display = "flex"
-    } else{
+    } else {
         pointer.style.display = "none"
     }
 }
 
-pointer.addEventListener('click',function() {
-    location.href = "#"
-})
+document.addEventListener('scroll', rolar);
 
-cardNaruto.addEventListener('click',function() {
-    img.setAttribute('src','assets/images/NARUTO-CORPOCOMPLETO.webp');
+document.querySelector('.btn-topo').addEventListener('click',() => location.href = "#")
+
+
+cardNaruto.addEventListener('click', function () {
+    img.setAttribute('src', 'assets/images/NARUTO-CORPOCOMPLETO.webp');
     nomePersonagem.textContent = "Naruto Uzumaki";
     nomePersonagem.style.color = "#FF6F00";
     descPersonagem.textContent = "O protagonista da série, conhecido por seu desejo de se tornar Hokage e por sua determinação inabalável. Ele possui uma natureza otimista e a habilidade de usar o chakra da raposa de nove caudas, Kurama.";
@@ -68,10 +69,10 @@ cardNaruto.addEventListener('click',function() {
     corpo.classList.remove('kakashi');
     corpo.classList.remove('madara');
     corpo.classList.remove('minato');
-    corpo.classList.add('naruto');
+    corpo.classList.add('naruto'); 
 })
-cardSasuke.addEventListener('click',function() {
-    img.setAttribute('src','assets/images/SASUKE-CORPOCOMPLETO.webp');
+cardSasuke.addEventListener('click', function () {
+    img.setAttribute('src', 'assets/images/SASUKE-CORPOCOMPLETO.webp');
     nomePersonagem.textContent = "Uchiha Sasuke";
     nomePersonagem.style.color = "#005CB2";
     descPersonagem.textContent = "Um dos últimos sobreviventes do clã Uchiha, rival de Naruto e amigo de infância. Ele busca vingança pela destruição de seu clã e passa por uma jornada de autodescoberta e redenção ao longo da série.";
@@ -88,8 +89,8 @@ cardSasuke.addEventListener('click',function() {
     fimcorpo.classList.remove('fmadara')
     fimcorpo.classList.remove('fminato')
 })
-cardSakura.addEventListener('click',function() {
-    img.setAttribute('src','assets/images/SAKURA-CORPOCOMPLETO.webp');
+cardSakura.addEventListener('click', function () {
+    img.setAttribute('src', 'assets/images/SAKURA-CORPOCOMPLETO.webp');
     nomePersonagem.textContent = "Sakura Haruno";
     nomePersonagem.style.color = "#FF69B4 ";
     descPersonagem.textContent = "Uma kunoichi talentosa e membro do Time 7. Inicialmente focada em Sasuke, Sakura evolui como ninja, desenvolvendo suas habilidades médicas e se tornando uma forte e inteligente combatente";
@@ -106,8 +107,8 @@ cardSakura.addEventListener('click',function() {
     fimcorpo.classList.remove('fmadara')
     fimcorpo.classList.remove('fminato')
 })
-cardKakashi.addEventListener('click',function() {
-    img.setAttribute('src','assets/images/KAKASHI-CORPOCOMPLETO.webp')
+cardKakashi.addEventListener('click', function () {
+    img.setAttribute('src', 'assets/images/KAKASHI-CORPOCOMPLETO.webp')
     nomePersonagem.textContent = "Hatake Kakashi"
     nomePersonagem.style.color = "#02376b"
     descPersonagem.textContent = "O líder do Time 7, conhecido por sua máscara e por ser um mestre ninja. Ele é habilidoso em diversas técnicas, incluindo o Chidori e o Sharingan."
@@ -124,8 +125,8 @@ cardKakashi.addEventListener('click',function() {
     fimcorpo.classList.remove('fmadara')
     fimcorpo.classList.remove('fminato')
 })
-cardMadara.addEventListener('click',function() {
-    img.setAttribute('src','assets/images/MADARA-CORPOCOMPLETO.webp')
+cardMadara.addEventListener('click', function () {
+    img.setAttribute('src', 'assets/images/MADARA-CORPOCOMPLETO.webp')
     nomePersonagem.textContent = "Uchiha Madara"
     nomePersonagem.style.color = "#8B0000 "
     descPersonagem.textContent = "Um lendário shinobi e um dos antagonistas principais da série. Ele é conhecido por sua busca pelo poder e por seu papel fundamental na história dos clãs ninja."
@@ -142,8 +143,8 @@ cardMadara.addEventListener('click',function() {
     fimcorpo.classList.add('fmadara')
     fimcorpo.classList.remove('fminato')
 })
-cardMinato.addEventListener('click',function() {
-    img.setAttribute('src','assets/images/MINATO-CORPOCOMPLETO.webp')
+cardMinato.addEventListener('click', function () {
+    img.setAttribute('src', 'assets/images/MINATO-CORPOCOMPLETO.webp')
     nomePersonagem.textContent = "Namikaze Minato"
     nomePersonagem.style.color = "#FFD700  "
     descPersonagem.textContent = "O Quarto Hokage de Konoha e pai de Naruto. Ele é famoso por suas habilidades como ninja, especialmente por ser o usuário original da técnica do Rasengan e por ser conhecido como o Relâmpago Amarelo de Konoha."
